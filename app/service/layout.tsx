9a5169817fb7899229c1759c7607d0ebf03431d0
Jakey Wakey <askjake331@gmail.com>
2023-12-09 22:37:24 -0500

    some changes

--- app/service/layout.tsx
+++ app/service/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import CONFIG from '../_config'
 import '../globals.css'
 import '../quiggle.css'
+import logo from '../_assets/logo.png'
+import Image from 'next/image'
 
 export const metadata: Metadata = {
   title: 'PBJ Service Co',
@@ -18,7 +20,20 @@ export default function RootLayout({
 
   return (
     <>
-			<nav className='primary bb-sm shadow-sm-down-black px-10 py-2 shadow-sm-in-up-gray'>PBJ</nav>
+			<nav className='primary bb-sm shadow-sm-down-black px-8 py-2 shadow-sm-in-up-gray ai-center flex jc-between'>
+				<div className="flex gap-2 ai-center">
+
+				<Image
+					className='primary-image'
+					src={ logo }
+					alt='PBJ Logo'
+					width={ 32 }
+				/>
+				<span className='bold text-lg ls-12 italic'>
+					PBJ Service Co
+				</span>
+				</div>
+			</nav>
 			{/* Content goes here */}{ children }
 		</>
   )
