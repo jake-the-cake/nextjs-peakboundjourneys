3a9a3a017008459f15c846cdb8d27daed54e9c76
Jakey Wakey <askjake331@gmail.com>
2023-12-12 16:32:46 -0500

    top section

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import CONFIG from '../_config'
 import '../globals.css'
 import '../quiggle.css'
+import Clock from '../_assets/Clock'
 
 export const metadata: Metadata = {
   title: 'PBJ Service Co',
@@ -13,6 +14,25 @@ export default function Home() {
   const { bg } = CONFIG.css
 
   return (
-		<div>Our Services:</div>
+    <>
+		<div className='flex ai-center p-6 gap-6'>
+      <div className='f-1'></div>
+      <div className="flex-center m-auto">
+        <div className="flex-col gap-1 relative pt-10">
+          <h1 className='primary px-2 ms-auto shadow-sm-down-black'>On Time.</h1>
+          <h1 className='accent px-2 shadow-sm-down-black'>Every Time.</h1>
+          <Clock
+            width='70%'
+            color='secondary'
+          />
+        </div>
+      </div>
+      <p className='f-3 text-dark ls-12 lh-15'>
+        We pride ourselves on our consistent track record of delivering services on time, every time. Timeliness is at the core of our operations, reflecting our dedication to ensuring that your experience with us is not only seamless but also punctual. When you choose PBJ, you're choosing a partner committed to meeting deadlines and surpassing expectations.
+      </p>
+      <div className='f-1'></div>
+    </div>
+		<div className='p-4 light'>Services</div>
+    </>
 	)
 }
