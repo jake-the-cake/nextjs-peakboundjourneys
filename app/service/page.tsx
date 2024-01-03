dec4c53c8629bb3c2596dc57c63795a43fdac987
Jakey Wakey <askjake331@gmail.com>
2024-01-02 20:22:05 -0500

    hmm

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -26,11 +26,11 @@ export default function Home() {
     </div>
     <div className='flex m-auto ai-center p-10 pt-0 gap-8 max-w-700'>
       <p className='f-1 text-dark ls-12 lh-15 text-center'>
-        We pride ourselves on our consistent track record of delivering services on time, every time. Timeliness is at the core of our operations, reflecting our dedication to ensuring that your experience with us is not only seamless but also punctual. When you choose PBJ, you are choosing a partner committed to meeting deadlines and surpassing expectations.
+        We've redefined what "Full Service" truly means. It's not just about completing the task at hand - it's about going above and beyond. Our commitment to excellence includes leaving no trace that we were there after each job is completed, fully understanding the importance of those finishing touches. Moreover, our dedication doesn't stop there; we're open to additional tasks upon request, provided our schedule allows.
       </p>
       <div className="flex-col gap-1 relative pt-10">
-        <h1 className='primary px-2 me-auto shadow-sm-down-black'>Full Service</h1>
-        <h1 className='accent px-2 shadow-sm-down-black'>Means FULL</h1>
+        <h1 className='primary px-2 me-auto shadow-sm-down-black'>Beyond</h1>
+        <h1 className='accent px-2 shadow-sm-down-black'>Full Service</h1>
         <Clock
           width={ '70%' }
           color={ 'secondary' }
