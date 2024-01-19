e0a954aa52653f747a46115ed3ad53d1aa2c8d59
Jakey Wakey <askjake331@gmail.com>
2024-01-19 09:11:29 -0500

    angies runnin

--- app/_data/index.ts
+++ app/_data/index.ts
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose'
 
-const { MONGODB_URI } = process.env
+const { NEXT_PUBLIC_MONGODB_URI } = process.env
 
-if (!MONGODB_URI) throw new Error('MONGODB_URI not defined')
+if (!NEXT_PUBLIC_MONGODB_URI) throw new Error('MONGODB_URI not defined')
 // if (!MONGODB_DB) throw new Error('MONGODB_DB not defined')
 
 let cached = global.mongoose
@@ -15,7 +15,7 @@ async function dbConnect() {
   if (cached.conn) return cached.conn
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI || '').then(mongoose => mongoose)
+    cached.promise = mongoose.connect(NEXT_PUBLIC_MONGODB_URI || '').then(mongoose => mongoose)
   }
 
   cached.conn = await cached.promise
