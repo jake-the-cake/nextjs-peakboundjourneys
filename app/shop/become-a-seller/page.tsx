b6a1c8221e93819fed7ce26d13be52fec39da8ab
Jakey Wakey <askjake331@gmail.com>
2024-01-22 19:38:23 -0500

    does this work

--- app/shop/become-a-seller/page.tsx
+++ app/shop/become-a-seller/page.tsx
@@ -1,4 +1,26 @@
-import React from 'react'
+'use client'
+import React, { FormEvent } from 'react'
+
+type ErrorLog = {
+	type?: string
+	message?: string
+}
+
+function validateRequired(x: any) {
+	console.log(x)
+}
+
+function handleNewStore(e: FormEvent) {
+	e.preventDefault()
+	const form: any = Object.fromEntries(new FormData(e.target as any))
+	console.log(form)
+	const error: ErrorLog[] = []
+	validateRequired(form.accountType)
+	fetch('/api/store/create', {
+		method: 'POST',
+		body: JSON.stringify(form)
+	}).then((data) => data.json()).then((data) => console.log(data))
+}
 
 export default function page() {
 	return (
@@ -8,16 +30,19 @@ export default function page() {
 				<p className='text-accent italic text-sm'>
 					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus perferendis doloribus dignissimos. Numquam veniam sit odit soluta quos vitae. Temporibus eum nemo error expedita assumenda impedit in enim ad dolorem.
 				</p>
-				<form className='mt-6 flex jc-center gap-6' action="">
-					<div className="flex-col">
-						<label htmlFor="company">Company Name</label>
-						<input className='accent-500' type="text" />
-						<div className="flex gap-4">
-							<input type="checkbox" name="no-business" id="no-business" />
-							<span>I am a personal seller</span>
-						</div>
+				<form className='mt-6 flex-col jc-center gap-6' onSubmit={ handleNewStore }>
+
+					{/* <div className="flex-col"> */}
+						{/* <label htmlFor="company">Company Name</label> */}
+						{/* <input className='accent-500' type="text" name="companyName" /> */}
+						{/* <div className="flex gap-4"> */}
+							{/* <input type="checkbox" name="no-business" id="no-business" /> */}
+							{/* <span>I am a personal seller</span> */}
+						{/* </div> */}
+					{/* </div> */}
+					<div>
+						<button type='submit' className='primary'>Submit</button>
 					</div>
-					<div>More</div>
 				</form>
 			</div>
 		</div>
